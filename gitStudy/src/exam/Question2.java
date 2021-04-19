5b6742a6efc8ead008cf4a83f1dc6e4e167222bd
S U Lee <dltmdtn3600@gmail.com>
2021-04-19 14:47:03 +0900

    branchB 업데이트 기념 커밋

--- gitStudy/src/exam/Question2.java
+++ gitStudy/src/exam/Question2.java
@@ -5,5 +5,10 @@ public class Question2 {
 		System.out.println(Math.random());
 		
 		System.out.println("Random!!");
+		
+		System.out.println("여기는 branchB 구역입니다.");
+		
+		// branchA,B 모두에 커밋을 2개 추가하고 history 내역을 보낼 것
+		// (단, Question2만 수정해야 함 ※ 이 주성은 branchB 에서 작성되었음)
 	}
 }

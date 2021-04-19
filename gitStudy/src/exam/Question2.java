c8325e7744f8b5d56bd4c780a7843fdd281fdaaf
S U Lee <dltmdtn3600@gmail.com>
2021-04-19 14:48:11 +0900

    branchB구역 두번째 업데이트 !

--- gitStudy/src/exam/Question2.java
+++ gitStudy/src/exam/Question2.java
@@ -8,6 +8,8 @@ public class Question2 {
 		
 		System.out.println("여기는 branchB 구역입니다.");
 		
+		System.out.println("branchB 구역 두번째 업데이트!");
+		
 		// branchA,B 모두에 커밋을 2개 추가하고 history 내역을 보낼 것
 		// (단, Question2만 수정해야 함 ※ 이 주성은 branchB 에서 작성되었음)
 	}

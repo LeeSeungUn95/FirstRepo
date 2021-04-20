c49df1fb9838fe0481be9d5d52ff191d0ad25131
S U Lee <dltmdtn3600@gmail.com>
2021-04-20 09:25:43 +0900

    commint branch C twice

--- gitStudy/src/exam/Answer1.java
+++ gitStudy/src/exam/Answer1.java
@@ -4,5 +4,7 @@ public class Answer1 {
 	public static void main(String[] args) {
 		System.out.println("Question1에 대한 정답 클래스");
 		System.out.println("branchC 에서 작업중");
+		
+		System.out.println("branch D가 작업할때 branch C도 놀고있지 않았음");
 	}
 }

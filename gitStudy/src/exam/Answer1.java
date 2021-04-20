d197b951e0a8e56fc764bcc4886849a5ae0b89f9
S U Lee <dltmdtn3600@gmail.com>
2021-04-20 09:26:57 +0900

    commit branch C three times

--- gitStudy/src/exam/Answer1.java
+++ gitStudy/src/exam/Answer1.java
@@ -6,5 +6,6 @@ public class Answer1 {
 		System.out.println("branchC 에서 작업중");
 		
 		System.out.println("branch D가 작업할때 branch C도 놀고있지 않았음");
+		System.out.println("C가 D보다 일을 더 잘해서 커밋을 두 번 했음");
 	}
 }
